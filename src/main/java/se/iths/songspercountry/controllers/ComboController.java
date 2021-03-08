793c5327734a084bb64556648c536058c2255817
SofHldn <sofie.holden@iths.se>
2021-03-08 09:45:40 +0100

    inlämning

--- src/main/java/se/iths/songspercountry/controllers/ComboController.java
+++ src/main/java/se/iths/songspercountry/controllers/ComboController.java
@@ -18,59 +18,21 @@ public class ComboController {
         private ComboService service;
 
 
-
-//        public ComboController(Service service) {
-//                this.service = service;
-//        }
-    //    @RequestMapping("/combo/")
-//    public String findOne(){
-//
-//        String jsonObject = new String(
-//                restTemplate.exchange("http://localhost:58081/countries", HttpMethod.GET, 	null, String.class).getBody());
-//        return jsonObject;
-
         @GetMapping("/combo")
         public List<Combo> listAll(){
 
-                Combo combo = new Combo("se", "sve", "hej", 1L);
-                service.createCombo(combo);
-                return service.listAllCombos();
+               return service.listAllCombos();
 
         }
 
-        @GetMapping("/combo/cc")
-        public Combo one(){
-                //var resultSongs = service.getSongs();
+        @GetMapping("/combo/{cc}")
+        public Combo one(@PathVariable String cc){
+
+                return service.getOne(cc).orElseThrow(()-> getStatusException(cc));
+
+       }
 
 
-                var resultCountries= service.getCountries();
-//                resultContries.orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND,
-//                        "id " + " Not Found Contries"));
-//                var result = resultSongs.toString() + resultContries.toString();
-//                System.out.println(result);
-                return resultCountries;
-
-
-        }
-
-
-
-
-//        @GetMapping("/combo/{cc}")
-//        public Combo findOne(@PathVariable String cc){
-//
-//        ResponseEntity<Country> responseEntity =
-//                new RestTemplate().getForEntity(
-//                        "http://localhost:52356/countries/", Country.class, cc);
-//
-//
-//        Combo combo = new Combo(responseEntity.getBody().getCountryCode(), responseEntity.getBody().getCountryName(), "hej", 1L);
-//                service.createCombo(combo);
-//        return service.getOne(cc)
-//                        .orElseThrow(()-> getStatusException(cc));
-//
-//
-//    }
         private ResponseStatusException getStatusException(String cc) {
 
                 return new ResponseStatusException(HttpStatus.NOT_FOUND,
--- src/main/java/se/iths/songspercountry/services/ComboService.java
+++ src/main/java/se/iths/songspercountry/services/ComboService.java
@@ -1,12 +1,13 @@
 package se.iths.songspercountry.services;
 
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 import se.iths.songspercountry.entities.Combo;
-import se.iths.songspercountry.entities.Country;
 import se.iths.songspercountry.repository.ComboRepository;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
@@ -20,44 +21,65 @@ public class ComboService implements se.iths.songspercountry.services.Service {
 
     }
 
-    @Override
-    public List<Combo> listAllCombos() {
-        return comboRepository.findAll();
-    }
 
     @Override
-    public Optional<Combo> getOne(String countryCode) {
-        return comboRepository.findById(countryCode);
-    }
-
-    @Override
-    public Combo createCombo(Combo combo) {
-
-        if (combo.getCountryName().isEmpty())
-            throw new RuntimeException();
-
-        return comboRepository.save(combo);
-    }
-
-
-    public Combo getCountries() {
+    public Optional<Combo> getOne(String cc) {
         System.out.println("json");
-        String url = "http://client-service/countries/cc";
-        String url1 = "http://client-service/songs/1";
+        String url = "http://localhost:7070/countries/" + cc;
+
+
+//        String url = "http://localhost:8500/v1/catalog/service/countries-service/countries/cc";
+//        String url1 = "http://localhost:8500/v1/catalog/service/songs/songs/1";
         RestTemplate restTemplateCountry = new RestTemplate();
         Combo country = restTemplateCountry.getForObject(url, Combo.class);
 
+        assert country != null;
+        String url1 = "http://localhost:6060/songs/" + country.getSongId();
         RestTemplate restTemplateSong = new RestTemplate();
         Combo song = restTemplateSong.getForObject(url1, Combo.class);
 
+        assert song != null;
         Combo combo = new Combo(country.getCountryCode(), country.getCountryName(), song.getTitle(),  country.getSongId());
 
 
         System.out.println(country);
         System.out.println(song);
         System.out.println(combo);
-        return combo;
+        return Optional.of(combo);
 
+
+    }
+
+    @Override
+    public Combo createCombo(Combo combo) {
+        if (combo.getCountryName().isEmpty())
+            throw new RuntimeException();
+
+        return comboRepository.save(combo);
+
+    }
+    @Override
+    public List<Combo> listAllCombos() {
+
+        String urlCountry = "http://localhost:7070/countries/";
+        String urlSong = "http://localhost:6060/songs/";
+        RestTemplate restTemplateCountry = new RestTemplate();
+        ResponseEntity<Combo[]> response = restTemplateCountry.getForEntity(urlCountry, Combo[].class);
+        Combo[] country = response.getBody();
+
+
+        List <Combo> list = new ArrayList<>();
+        for (Combo c: country) {
+
+            RestTemplate restTemplateSong = new RestTemplate();
+            Combo song = restTemplateSong.getForObject(urlSong+c.getSongId(), Combo.class);
+            Combo combo = new Combo(c.getCountryCode(), c.getCountryName(), song.getTitle(), c.getSongId());
+
+            list.add(combo);
+
+        }
+
+        return list;
 
     }
 
